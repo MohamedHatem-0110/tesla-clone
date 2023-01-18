9d369e755cc2bc553da1abf130907b813683db9f
Mohamed Hatem <83113802+MohamedHatem-0110@users.noreply.github.com>
2023-01-18 18:34:14 +0200

    Creating app

---
+++ tesla-clone/src/components/Section.js
@@ -0,0 +1,12 @@
+import React from "react";
+import styled from "styled-components";
+function Section() {
+  return <Wrap>Section</Wrap>;
+}
+const Wrap = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background: orange;
+`;
+
+export default Section;
